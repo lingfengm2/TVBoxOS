02b587d95ac4e4b745dc729a090e2d0ecec6c884
lingfengm2 <123752197+lingfengm2@users.noreply.github.com>
2023-08-30 21:57:02 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -127,7 +127,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "http://i6m.cn:88/tv/18tv/gaitv.json");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
